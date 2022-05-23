b437e70ec0d039c14cdad39519d27f4d0ed41f5f
prem9528 <101805758+prem9528@users.noreply.github.com>
2022-05-23 17:00:59 +0530

    Add files via upload

---
+++ src/models/bookModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    bookCover:{
+        type:String,
+        required:"bookcover is required",
+        unique:true
+    },
+    excerpt: {
+        type: String,
+        required: true,
+    },
+    userId: { type: mongoose.Types.ObjectId, ref: 'User' },
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    subcategory: {
+        type: String,
+        required: true,
+    },
+    reviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    deletedAt: Date,
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema)
